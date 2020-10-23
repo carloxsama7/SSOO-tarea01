0ef9b1db9cb6d16dde7ae23daf8ff4684dd01a58
carloxsama7 <carloxsama7@gmail.com>
2020-10-23 19:59:18 -0300

    cambio funcionamiento de las señales y comentarios

--- src/hola.c
+++ src/hola.c
@@ -3,44 +3,29 @@
 #include <signal.h>
 #include <time.h>
 #include <stdlib.h>
-/* LIBRERIAS PROFESOR
-#include <random>
-#include <iostream>    // for cout
-#include <fstream>
-#include <queue>       // for queue
-#include <vector>      // for vector
-#include <array>
-#include <future>
-#include <thread>         // std::this_thread::sleep_for
-#include <chrono>         // std::chrono::seconds
-#include <csignal>
 
+int ejecuciones = 0;
 
-*/
-int trials = 0;
 void mostrar_hora(){
 	time_t hora;
 	time(&hora);
 	printf("\nSeñal SIGUSR1 recibida: %s\n", ctime(&hora));
 }
-void detener( int signal_num ) { 
-		
-	trials++;
-	//printf("Interrupt signal is (%d)\n",signal_num); 
+void detener( int numero_senal ) { 
+	ejecuciones++;
 	mostrar_hora();
-	if(trials == 2){
-		exit(signal_num);  
+	if(ejecuciones == 2){
+		exit(numero_senal);  
 	} 
 } 
 
 int main(int argc,char* argv[]) {
 	int pid=getpid();
-
 	printf("Programa hora ejecutandose. PID: %d.\n", pid);
 	while(1){
 		printf("Listo para recibir la señal SIGUSR1\n");
-		signal (SIGINT , mostrar_hora);
-		signal(SIGTERM, detener);
+		signal (SIGINT , detener);
+		signal(SIGTERM, mostrar_hora);
 		pause();
 	}
 	return 0; 
