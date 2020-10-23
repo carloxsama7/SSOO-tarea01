5237212b6269c3b0c60976a6bbc84f148ac45c0e
carloxsama7 <carloxsama7@gmail.com>
2020-10-23 18:40:12 -0300

    Carpeta codigo profesor modificado

--- src/hola.c
+++ src/hola.c
@@ -1,6 +1,35 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-int main(int argc, char const *argv[]){
-    printf("adasdsd");
-    return 0;
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+/* LIBRERIAS PROFESOR
+#include <random>
+#include <iostream>    // for cout
+#include <fstream>
+#include <queue>       // for queue
+#include <vector>      // for vector
+#include <array>
+#include <future>
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
+#include <csignal>
+
+
+*/
+
+void mostrar_hora(){
+	time_t hora;
+	time(&hora);
+	printf("\nSeñal SIGUSR1 recibida: %s\n", ctime(&hora));
+}
+
+int main(int argc,char* argv[]) {
+	int pid=getpid();
+
+	printf("Programa hora ejecutandose. PID: %d.\nListo para recibir la señal SIGUSR1\n", pid);
+	while(1){
+		signal (SIGINT , mostrar_hora);
+		pause();
+	}
+	return 0; 
 }
